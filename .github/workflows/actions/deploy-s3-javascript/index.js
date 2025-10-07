49b9f5ed6096515241ba54ab01b6909e9098e332
Amine <amine_mohammad@ecssr.ae>
2025-10-07 11:25:27 +0400

    added bucket url

--- .github/workflows/actions/deploy-s3-javascript/index.js
+++ .github/workflows/actions/deploy-s3-javascript/index.js
@@ -10,17 +10,18 @@ const run = async () => {
         const accessKeyId = core.getInput('aws-access-key-id', { required: true });
         const secretAccessKey = core.getInput('aws-secret-access-key', { required: true });
         const sourceDir = core.getInput('source-dir', { required: true }) || 'dist';
-      await exec.exec('aws', ['s3','sync',`${sourceDir}/`, `s3://${bucketName}/`,'--delete'], 
-        {
-            env: {
-                ...process.env,
-                AWS_ACCESS_KEY_ID: accessKeyId,
-                AWS_SECRET_ACCESS_KEY: secretAccessKey,
-                AWS_DEFAULT_REGION: region,
-              }
-        });
-        
+        await exec.exec('aws', ['s3','sync',`${sourceDir}/`, `s3://${bucketName}/`,'--delete'], 
+            {
+                env: {
+                    ...process.env,
+                    AWS_ACCESS_KEY_ID: accessKeyId,
+                    AWS_SECRET_ACCESS_KEY: secretAccessKey,
+                    AWS_DEFAULT_REGION: region,
+                }
+            });
+
         core.notice('Deployment to S3 completed successfully.');
+        core.setOutput('bucket-url' , `https://${bucketName}.s3.${region}.amazonaws.com/`);
     } catch (error) {
         core.setFailed(`Deployment failed: ${error.message}`);
     }
